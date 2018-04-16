e56e1a27b6779f109d9e0c9114b3492e0e92caf7
Cat <cchiong1597@gmail.com>
2018-04-16 01:35:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(playlist, artist, song) {
 playlist[artist] = song
 return playlist
 }
-function removeFromPlaylist (playlist, artistName)
+function removeFromPlaylist (playlist, artistName) {
 delete playlist[artistName]
 return playlist}
